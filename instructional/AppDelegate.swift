2c367610b5c9b5ff431e2b2b0a99392ae1bb5800
Marton Kerekes <kerekes.j.marton@gmail.com>
2020-05-25 11:29:05 +0200

    cleanup

--- instructional/AppDelegate.swift
+++ instructional/AppDelegate.swift
@@ -3,14 +3,14 @@ import UIKit
 @UIApplicationMain
 class AppDelegate: UIResponder, UIApplicationDelegate {
 
+    var tasks: AppTasks = AppTasks()
+    
     func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey: Any]?) -> Bool {
         // Override point for customization after application launch.
         return true
     }
 
     // MARK: UISceneSession Lifecycle
-    
-    var tasks: AppTasks = AppTasks()
     
     func application(_ application: UIApplication, configurationForConnecting connectingSceneSession: UISceneSession, options: UIScene.ConnectionOptions) -> UISceneConfiguration {
         // Called when a new scene session is being created.
